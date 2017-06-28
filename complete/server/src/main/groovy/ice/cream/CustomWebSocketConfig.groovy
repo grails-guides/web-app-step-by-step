531ad23bf055c3e493324928d5a0eab9cda649d9
sdelamo <sergio.delamo@softamo.com>
2017-06-28 18:28:14 +0200

    remove println statement

--- complete/server/src/main/groovy/ice/cream/CustomWebSocketConfig.groovy
+++ complete/server/src/main/groovy/ice/cream/CustomWebSocketConfig.groovy
@@ -1,11 +1,13 @@
 package ice.cream
 
 import grails.plugin.springwebsocket.DefaultWebSocketConfig
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 
+@Slf4j
 @Configuration
 @EnableWebSocketMessageBroker
 class CustomWebSocketConfig extends DefaultWebSocketConfig {
@@ -15,7 +17,7 @@ class CustomWebSocketConfig extends DefaultWebSocketConfig {
 
     @Override
     void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) { //<2>
-        println "registerStompEndpoints with allowedOrigin: ${allowedOrigin}"
+        log.info 'registerStompEndpoints with allowedOrigin: {}', allowedOrigin
         stompEndpointRegistry.addEndpoint("/stomp").setAllowedOrigins(allowedOrigin).withSockJS()
     }
 }
